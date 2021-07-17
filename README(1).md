f577259d3ee3f2855d2eb2dab3ec2db13f153b7d
Darsh-coder <75057254+Darsh-coder@users.noreply.github.com>
2021-07-17 21:16:52 +0530

    Add files via upload

---
+++ README(1).md
@@ -0,0 +1,4 @@
+# p5.play-boilerplate
+Boiler plate for p5.play
+
+https://darsh-coder.github.io/pac-man-/
--- sketch.js
+++ sketch.js
@@ -5,9 +5,10 @@ var apple,  strawberry ,cherry, orange
 var lolipop,icecream
 var appleimg,  strawberryimg ,cherryimg, orangeimg
 var lolipopimg,icecreamimg
+var wall,wall1,wall2,wall3,wall4,wall5,wall6,wall7,wall8,wall9,wall10,wall11,wall12,wall13,wall14,wall15,wall16,wall17,wall18,wall19,wa2l,wall21,wall22,wall23,wall25,wall26,wall27,wall28,wall29,wall30,wall31,wall32
 function setup() {
-  createCanvas(800,400);
-  ghost2= createSprite(200, 200, 50, 50);
+  createCanvas(900,500);
+  /*ghost2= createSprite(200, 200, 50, 50);
   ghostimg2 = loadImage("g2.png");
   ghost2.addImage("ghost2", ghostimg2)
 
@@ -35,7 +36,6 @@ cherry.addImage("cherrypple", cherryimg)
 strawberry= createSprite(300, 200, 50, 50);
 strawberryimg = loadImage(" strw.png");
 strawberry.addImage("strawberry", strawberryimg)
-
 orange= createSprite(300, 200, 50, 50);
 orangeimg = loadImage(" or.png");
 orange.addImage("orange", orangeimg)
@@ -47,13 +47,54 @@ lolipop.addImage("lolipop", lolipopimg)
 
 icecream= createSprite(300, 200, 50, 50);
 icecreamimg = loadImage(" ice.png");
-icecream.addImage("icecream", icecreamimg)
+icecream.addImage("icecream", icecreamimg)*/
+
+wall= createSprite(10, 10, 50, 600);
+wall1= createSprite(10,130, 400, 35);
+wall2= createSprite(100, 90, 35, 50);
+wall3= createSprite(282.2, 60, 400, 35);
+wall4= createSprite(192.5, 200, 35, 120);
+wall5= createSprite(330, 110, 35, 130);
+wall6= createSprite(17, 450, 35, 160);
+wall7= createSprite(30, 385, 360, 35);
+wall8= createSprite(192.5, 350, 35, 50);
+wall9= createSprite(110, 330, 35, 250);
+
+wall10= createSprite(300, 480, 220, 35);
+wall11= createSprite(470,455,35, 90);
+wall12= createSprite(500, 427.5, 60, 35);
+wall13= createSprite(630, 500, 35, 100);
+wall14= createSprite(882, 500, 35, 180);
+wall15= createSprite(330, 110, 35, 130);
+wall16= createSprite(890, 420, 200, 35);
+wall17= createSprite(890, 10, 35, 200);
+wall18= createSprite(880, 100, 300, 35);
+wall19= createSprite(747.5, 160, 35, 120);
+
+
+wall20= createSprite(810, 65, 35, 50);
+wall21= createSprite(750,205, 200, 35);
+wall22= createSprite(667.5, 235, 35, 90);
+wall23= createSprite(760, 260, 35, 90);
+
+wall25= createSprite(792.5, 290, 100, 35);
+wall26= createSprite(570, 50, 35, 250);
+wall27= createSprite(512.5, 190, 150, 35);
+wall28= createSprite(520, 250, 35, 150);
+wall29= createSprite(395, 315, 35, 100);
+wall30= createSprite(352.5,250,120, 35);
+wall31= createSprite(352.5, 350, 120, 35);
+wall32= createSprite(350,390,35, 50);
+
+
+
+
 
 
 }
 
 function draw() {
-  background(255,255,255);  
+  background(0);  
   
   
   drawSprites();
